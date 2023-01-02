1db3e752b63949c01947cddd852b75130055bebf
Barnabas Bulpett <barnabasbulpett@gmail.com>
2023-01-02 13:42:10 -0500

    Create choropleth from Plotly Express

--- main.py
+++ main.py
@@ -3,22 +3,85 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc, html
 from dash.dependencies import Input, Output
 
 app = dash.Dash(__name__)
 
-# Import and clean data (import CSV into pandas)
+# Import and clean data (import CSV into pandas dataframe)
 df = pd.read_csv("bees.csv")
 df = df.groupby(
-  ['State', 'ANSI', 'Affected by', 'Year', 'state_code']
-  )[
-    ['Pct of Colonies Impacted']
-  ].mean()
+        ['State', 'ANSI', 'Affected by', 'Year', 'state_code']
+    )[
+        ['Pct of Colonies Impacted']
+    ].mean()  # Gets average of colonies impacted for each column
 
 df.reset_index(inplace=True)
 print(df[:5])
 
-# App layout
+# App layout (Dash components and html - graphs, checkboxes, etc.)
+# http://dash.plotly.com/dash-core-components
+app.layout = html.Div([
+    html.H1("US Bee Population Impacts", style={'text-align': 'center'}),
+    dcc.Dropdown(
+        id='slct_year',
+        options=[
+            # Year values are integers
+            {"label": "2015", "value": 2015},
+            {"label": "2016", "value": 2016},
+            {"label": "2017", "value": 2017},
+            {"label": "2018", "value": 2018},
+        ],
+        multi=False,
+        value=2015,  # Initial value
+        style={'width': '40%'}
+    ),
+    html.Div(id='output_container', children=[]),
+    html.Br(),
+    dcc.Graph(id='my_bee_map', figure={})
+])
 
+# Define callback to connect the Ployly graphs with Dash Components
+@app.callback(
+    [
+        Output(component_id='output_container', component_property='children'),
+        Output(component_id='my_bee_map', component_property='figure')
+    ],
+    [
+        Input(component_id='slct_year', component_property='value')
+    ]
+)
+
+# Each callback needs a function. Argument matches the component property.
+def update_graph(option_slctd):
+    print(option_slctd)
+    print(type(option_slctd))
+
+    container = "The year chosen by user was: {}".format(option_slctd)
+
+    # Make a copy of the dataframe
+    dff = df.copy()
+    dff = dff[dff["Year"] == option_slctd]  # Defaults to default value
+    dff = dff[dff["Affected by"] == "Varroa_mites"]
+
+    # Plotly Express
+    # https://plotly.com/python-api-reference/generated/plotly.express.choropleth.html
+    fig = px.choropleth(
+        data_frame=dff,
+        locationmode='USA-states',
+        locations='state_code',
+        scope="usa",
+        color='Pct of Colonies Impacted',
+        hover_data=['State', 'Pct of Colonies Impacted'],
+        color_continuous_scale=px.colors.sequential.YlOrRd,
+        labels={'Pct of Colonies Impacted': '% of Bee Colonies'},
+        template='plotly_dark'
+    )
+
+    # What is returned here actually goes into the outputs of the callback
+    # (arguments are POSITIONAL)
+    return container, fig
+
+# Run server
+if __name__ == '__main__':
+    app.run_server(debug=True)
